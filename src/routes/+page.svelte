63bb38af3264be32f1b40712f3b7e8331dd4da51
Eriseldi <eribeq21@htl-shkoder.com>
2026-03-04 12:41:39 +0100

    Second commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script>
-    import car from '$lib/assets/car.jpg';
+    import car from '$lib/assets/car.webp';
 </script>
+
+<svelte:head>
+    <title>Rolls Royce</title>
+    <meta name="description" content="Discover the epitome of luxury and craftsmanship with Rolls Royce. Explore our exquisite collection of vehicles that redefine elegance and performance. Experience the unparalleled blend of opulence and engineering excellence that only Rolls Royce can offer.">
+</svelte:head>
 
 <header class="h-125 bg-center bg-cover flex items-center align-center justify-center" style="background-image: url({car})">
     <h1 class="text-3xl text-white">Rolls Royce</h1>
@@ -12,5 +17,5 @@
 
 
 <footer>
-    
+
 </footer>
