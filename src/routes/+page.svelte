6ae6e12ad6d4b62a3d54ec0d9fe093499bcd8936
Eriseldi <eribeq21@htl-shkoder.com>
2026-03-04 12:28:19 +0100

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import car from '$lib/assets/car.jpg';
+</script>
+
+<header class="h-125 bg-center bg-cover flex items-center align-center justify-center" style="background-image: url({car})">
+    <h1 class="text-3xl text-white">Rolls Royce</h1>
+</header>
+
+<main>
+
+</main>
+
+
+<footer>
+    
+</footer>
