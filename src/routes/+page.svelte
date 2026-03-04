2b89f16f1583d9646da11d30f90d614ba9de93f9
Eriseldi <eribeq21@htl-shkoder.com>
2026-03-04 12:52:56 +0100

    Updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,16 @@
 <script>
-    import car from '$lib/assets/car.jpg';
+    import car2 from '$lib/assets/car2.jpg';
 </script>
 
 <svelte:head>
     <title>Rolls Royce</title>
     <meta name="description" content="Discover the epitome of luxury and craftsmanship with Rolls Royce. Explore our exquisite collection of vehicles that redefine elegance and performance. Experience the unparalleled blend of opulence and engineering excellence that only Rolls Royce can offer.">
+    <meta property="og:title" content="Rolls Royce">
+    <meta property="og:description" content="Discover the epitome of luxury and craftsmanship with Rolls Royce. Explore our exquisite collection of vehicles that redefine elegance and performance. Experience the unparalleled blend of opulence and engineering excellence that only Rolls Royce can offer.">
+    <meta property="og:image" content="{car2}"> 
 </svelte:head>
 
-<header class="h-125 bg-center bg-cover flex items-center align-center justify-center" style="background-image: url({car})">
+<header class="h-125 bg-center bg-cover flex items-center align-center justify-center" style="background-image: url({car2})">
     <h1 class="text-3xl text-white">Rolls Royce</h1>
 </header>
 
@@ -16,6 +19,6 @@
 </main>
 
 
-<footer>
-
+<footer class="h-125 bg-gray-800 text-white flex items-center align-center justify-center">
+    <p>Test-Page for SEO</p>
 </footer>
